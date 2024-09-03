ed88dc731d45a2239574e2e5e8b7de8265be1061
NovikovaAleAle <novikova.ale.ale@gmail.com>
2024-09-03 21:21:49 +0300

    create test for weather model

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -2,13 +2,13 @@ class Weather < ApplicationRecord
 
   def self.weather_historical_arr(count)
     temperature_arr = []
-    Weather.limit(count).each{ |el| temperature_arr << el.temperature }
+    Weather.order(epoch_time: :desc).limit(count).reverse_each{ |el| temperature_arr << el.temperature }
     temperature_arr
   end
 
   def self.weather_historical_hash(count)
     temperature_hash = {}
-    Weather.limit(count).each{ |el| temperature_hash[Time.at(el.epoch_time).to_datetime] = el.temperature }
+    Weather.order(epoch_time: :desc).limit(count).reverse_each{ |el| temperature_hash[Time.at(el.epoch_time).to_datetime] = el.temperature }
     temperature_hash
   end
 end
